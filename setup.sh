d40907d202083d4637e08c3b92dc18eccdb99c34
Scott Meyers <scott@siberia.io>
2021-06-10 15:13:33 -0400

    adds emacs to setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ CWD="$PWD"
 ln -sf $CWD/git/.gitconfig ~/.gitconfig
 ln -sf $CWD/git/.gitignore_global ~/.gitignore_global
 
+ln -sf $CWD/emacs/.emacs.d ~/.emacs.d
+
 ln -sf $CWD/vim/.vim ~/.vim
 ln -sf $CWD/vim/.vimrc ~/.vimrc
 
